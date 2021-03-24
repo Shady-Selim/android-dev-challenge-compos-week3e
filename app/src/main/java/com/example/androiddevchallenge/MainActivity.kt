ebcdbb2766780a690930e807099e80dd1ce01af2
Shady Selim <shady.selim@hotmail.com>
2021-03-24 11:37:35 +0200

    UI updates

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,21 +18,21 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.androiddevchallenge.data.models.Weather
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.main.MainViewModel
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.typography
 import dev.chrisbanes.accompanist.coil.CoilImage
 import kotlin.math.ceil
 import kotlin.math.floor
@@ -55,16 +55,81 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp(model: MainViewModel = MainViewModel()) {
     val context = LocalContext.current
-    val weather by model.getWeather("${context.resources.getString(R.string.lat)},${context.resources.getString(R.string.log)}").observeAsState()
-    Surface(color = MaterialTheme.colors.background) {
-        CoilImage(
-            data = "https://darksky.net/images/weather-icons/${weather?.currently?.icon}.png",
-            contentDescription = "My content description"
-        )
-        /*Image(
-            imageResource(R.drawable.actual_png_file),
-            contentDescription = "description of the image"
+    val contextResourses = context.resources
+    val weather by model.getWeather(
+        "${contextResourses.getString(R.string.lat)},${
+            contextResourses.getString(
+                R.string.log
+            )
+        }"
+    ).observeAsState()
+    Surface(
+        color = MaterialTheme.colors.background, // todo: replace with video
+        modifier = Modifier.fillMaxHeight()
+    ) {
+        Card(
+            elevation = 4.dp,
+            modifier = Modifier
+                .padding(96.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Row {
+                weather?.let {
+                    CoilImage(
+                        data = "https://darksky.net/images/weather-icons/${it.currently.icon}.png",
+                        contentDescription = "Weather icon status: ${it.currently.icon}",
+                        modifier = Modifier
+                            .width(128.dp)
+                            .height(128.dp)
+                    )
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+
+                        Text(
+                            text = it.timezone,
+                            style = typography.h5
+                        )
+                        Text(text = it.daily.summary)
+                        Text(text = it.currently.summary)
+                        Text(
+                            text = contextResourses.getString(
+                                R.string.high,
+                                ceil(it.daily.data[0].temperatureHigh).toInt().toString()
+                            )
+                        )
+                        Text(
+                            text = contextResourses.getString(
+                                R.string.low,
+                                floor(it.daily.data[0].temperatureLow).toInt().toString()
+                            )
+                        )
+
+                    }
+                    Column {
+                        Text(
+                            text = contextResourses.getString(
+                                R.string.temp,
+                                it.currently.temperature.roundToInt().toString()
+                            ),
+                            style = typography.h3
+                        )
+                        Text(
+                            text = contextResourses.getString(
+                                R.string.feels_like,
+                                it.currently.apparentTemperature.roundToInt().toString()
+                            )
+                        )
+                    }
+
+                }
+            }
+            /*Image(
+            painter = painterResource(R.drawable.your_drawable),
+            contentDescription = "Content description for visually impaired"
         )*/
+        }
     }
 }
 
